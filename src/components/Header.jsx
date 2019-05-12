0125f83f64d91ebd855cfb114e90140dbaa4cd08
gilardh <gilardh@yahoo.com>
2019-05-12 03:02:19 +0200

    Update of Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import NavItem from 'react-bootstrap/NavItem'
+//import Nav from 'react-bootstrap/Nav'
+//import NavItem from 'react-bootstrap/NavItem'
 
-import logoFull from "../logo_full.png";
+import logo from "../images/hush_logo_white.svg";
 
 export default class Header extends React.Component {
     render() {
         return (
-            <Navbar id="header" className="zenHeader">
-                <div className="container">
-                        <Navbar.Brand>
-                            <img src={logoFull} alt="logo"/>
-                        </Navbar.Brand>
-                    <Nav pullRight>
-                        <NavItem href="https://myzenwallet.io/" className="headerLeftOption">WEB WALLET</NavItem>
-                        <Navbar.Text>|</Navbar.Text>
-                        <NavItem href="http://getzen.cash/" className="headerLeftOption">FAUCET</NavItem>
-                        <Navbar.Text>|</Navbar.Text>
-                        <NavItem href="https://support.horizen.global" className="headerLeftOption">SUPPORT</NavItem>
-                    </Nav>
+            <Navbar id="header" className="zenHeader" bg="dark" variant="dark">
+            <div className="container">
+                <Navbar.Brand href="#home">
+                <img alt="HUSH" src={logo} width="30" height="30" className="d-inline-block align-top"/>
+                {' HUSH Paper Wallet'}
+                </Navbar.Brand>
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>Source code : <a href="https://github.com/MyHush/paper.myhush.org">Github</a></Navbar.Text>
+                </Navbar.Collapse>
                 </div>
             </Navbar>
         );
